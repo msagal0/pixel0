dfe39bf9c53cb732f71baa3dc759001672375038
Your Name <martin.sagalovskiy@gmail.com>
2025-09-03 20:35:23 -0400

    View Save mode

--- script.js
+++ script.js
@@ -7,11 +7,46 @@ window.onload = () => {
     const drawColorBox = document.getElementById('drawColorBox');
     const defaultButton = document.getElementById('default');
     const enter = document.getElementById('enter');
+    const nameBox = document.getElementById("nameBox");
+    const saveButton = document.getElementById('save');
+    const openText = document.getElementById('openText');
+    const openButton = document.getElementById('openButton');
     inside.appendChild(canvas);
     const ctx = canvas.getContext("2d");
     canvas.width = 500;
     canvas.height = 500;
     setDefault();
+    let currentSquares = [];
+    let everything = [];
+
+    saveButton.onclick = () => {
+        everything = [];
+        everything.push(currentSquares);
+        everything.push(lineColorBox.value);
+        everything.push(gridColorBox.value);
+
+        console.log(everything);
+
+        localStorage.setItem(nameBox.value, JSON.stringify(everything));
+    }
+
+    openButton.onclick = () => {
+        try {
+            let coords = JSON.parse(localStorage.getItem(openText.value))[0];
+            ctx.fillStyle = everything[1];
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            for (let x = 0; x < 50; x++) {
+                for (let y = 0; y < 50; y++) {
+                    drawSquare(x, y, 10, 1, everything[2]);
+                }
+            }
+            for (const coord of coords) {
+                drawSquare(coord[0], coord[1], 10, 1, drawColorBox.value);
+            }
+        } catch (err) {
+            console.log("Error:", err.name)
+        }  
+    }
 
     //Customize grid color
     enter.addEventListener('mousedown', () => {
@@ -39,6 +74,10 @@ window.onload = () => {
     //Default event listener
     defaultButton.addEventListener('mousedown', setDefault);
 
+    function coordInList(coord, list) {
+        return list.find((elem) => (elem[0] === coord[0] && elem[1] === coord[1]));
+    }
+
     //Function to draw squares
     function drawSquare(x, y, size, space, color) {
         ctx.fillStyle = color;
@@ -53,15 +92,19 @@ window.onload = () => {
     for (let x = 0; x < 50; x++) {
         for (let y = 0; y < 50; y++) {
             drawSquare(x, y, 10, 1, gridColorBox.value);
-        } 
+        }
     }
     //mousedown event listener on the canvas to draw
-    canvas.addEventListener('mousedown', (de) => {
+    canvas.addEventListener('mousedown', () => {
         //Function to draw while mouse is moving
         function mouseMove (me) {
             let x = Math.floor(me.offsetX / 10);
             let y = Math.floor(me.offsetY / 10);
             drawSquare(x, y, 10, 1, drawColorBox.value);
+            if (!(coordInList([x, y], currentSquares))) {
+                currentSquares.push([x, y]);
+                console.log([x, y] in currentSquares);
+            }
         }
         //Function to stop drawing
         function mouseUp () {
